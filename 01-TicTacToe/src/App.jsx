6cf65e8dd540a6d62fac7d545c9edac1a516a41b
ubuntu <ubuntu@ubuntu.myguest.virtualbox.org>
2023-11-15 14:24:34 +0100

    PrimerCommit

---
+++ 01-TicTacToe/src/App.jsx
@@ -0,0 +1,41 @@
+import {useState} from "react"
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+const Square = ({children, updateBoard, index}) => {
+  return(
+    <div className="square">
+      {children}
+    </div>
+  )
+}
+
+function App() {
+  const [board, setBoard] = useState(Aray(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+
+    return( 
+      <main className='board'>
+        <h1>Tic Tac Toe</h1>
+        <section className="game">
+          {
+            board.map((_, index) => {
+              return(
+                <Square
+                key={index}
+                index={index}
+                >
+                  {board[index]}
+                </Square>
+              )
+            })
+          }
+        </section>
+      </main>
+    )
+}
+
+export default App
