c49fa8dff777e12688a904a78f28f138fe653ae8
David <davidrr.dev@gmail.com>
2023-12-13 14:24:49 +0100

    aplicacion del tiempo form y img

---
+++ 01-reactWeather/src/components/WeatherForm.jsx
@@ -0,0 +1,17 @@
+
+
+export function Formulario(){
+    return(
+        <div>
+            <form action="">
+                <input className='w-75' type="text" placeholder="Search for a city..."/>
+                <input type="button" value="" />
+            </form>
+            
+
+
+        </div>
+    )
+
+
+}
---
+++ 01-reactWeather/src/components/vistaTiempo.jsx
@@ -0,0 +1,57 @@
+import React from "react"
+export function Cartas(){
+    return(
+      <div>
+        <div className="card col-9 m-auto mt-5">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="card-link">Card link</a>
+              <a href="#" className="card-link">Another link</a>
+            </div>
+        </div>
+        
+        <div class="row w-75 m-auto mt-5">
+            <div class="col-sm-3 mb-3 mb-sm-0">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+          
+            <div class="col-sm-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    )
+
+}
