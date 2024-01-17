5011b560620707f0a8644b51b5ea69e75d9a73e1
David <davidrr.dev@gmail.com>
2024-01-17 14:15:49 +0100

    modifica el nav

--- pokedexDavidAEA2/src/components/navegador.jsx
+++ pokedexDavidAEA2/src/components/navegador.jsx
@@ -1,14 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function atras(){
 
 
 }
+
 export default function Navegador(){
     const [mostrarFormulario, setMostrarFormulario] = useState(false);
     const toggleFormulario = () => {
         setMostrarFormulario(!mostrarFormulario);
       };
+    const [buscar,setBuscar]=useState('');
+    const URL="https://pokeapi.co/api/v2/pokemon/"+buscar;
+    const getData = async () => {
+        await fetch(URL)
+          .then(response => {return response.json()})
+          .then( data => {
+            if(data.cod >= 400) {
+              setValues(false)
+            }else{         
+              setImagen(data.weather[0].main)
+              setValues(data)
+            }        
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      }
+      const handleSearch = (e) => {
+        if(e.key === 'Enter'){      
+          setBuscar(e.target.value)
+        }
+      }
+      useEffect(()=>{
+        getData()
+      },[buscar]) 
     return( 
     <div className="fixed-top ">
         <header className="d-flex">
@@ -16,10 +42,10 @@ export default function Navegador(){
                 <button className="bg-white border-bottom text-start " onClick={atras}> <img src="src/assets/img/atras.svg" alt="" className="start-0 " /></button>
             </div> 
             <div className="w-50 d-flex "> 
-                <button className="bg-white align-middle " onClick={toggleFormulario}><img src="src/assets/img/search.svg" alt="" /></button>
+                <button className="bg-white  " onClick={toggleFormulario}><img src="src/assets/img/search.svg" alt="" /></button>
                 {mostrarFormulario && (
-                <form className="w-100 align-middle ">
-                <input type="text" id="campoTexto" name="campoTexto" className="align-middle w-100"/>
+                <form className="w-100 medio ">
+                <input type="text" id={handleSearch} name="campoTexto" className=" w-75"/>
                 </form>
             )}
             </div>
