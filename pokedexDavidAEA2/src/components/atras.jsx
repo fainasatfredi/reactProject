121bce343d5d40cb2c067ea943820d756c04bcbf
David Rojas Rodríguez <davidrr.dev@gmail.com>
2024-01-24 14:19:34 +0100

    navbarbueno

--- pokedexDavidAEA2/src/components/atras.jsx
+++ pokedexDavidAEA2/src/components/atras.jsx
@@ -1,1 +1,102 @@
-<button className="bg-white border-bottom text-start " onClick={atras}> <img src="src/assets/img/atras.svg" alt="" className="start-0 " /></button>
+<div>
+    <button className="bg-white border-bottom text-start " onClick={atras}> <img src="src/assets/img/atras.svg" alt="" className="start-0 " /></button>
+    <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <img src="scr/assets/img/pokeball.png" alt="" />
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <button >
+                            <h3>Kanto</h3>
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button  >
+                            <h3>Johto</h3>
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button  >
+                            <h3>Hoenn</h3>
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button className=" text-start " >
+                            <h3>Sinnoh</h3>
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button  >
+                            <h3>Unova</h3>
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button  >
+                            <h3>Kalos</h3>
+                        </button>
+                    </li>
+                </ul>
+                <div className="w-50 d-flex "> 
+                <button className="bg-white border-black circulo " onClick={toggleFormulario}><img src="src/assets/img/search.svg" alt="" /></button>
+                {mostrarFormulario && (
+                <form className="w-100 medio ">
+                <input type="text" id={handleSearch} name="campoTexto" className="inform ms-3 "/>
+                </form>
+            )}
+            </div>
+            </div>
+        </div>
+    </nav>
+    </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    <div className="fixed-top bg-black">
+      
+        <header className="d-flex">
+            <div className="w-75">  
+                <li className="nav-item"><button >
+                  <h3>Kanto</h3>
+                </button></li>
+                <li className="nav-item"><button  >
+                  <h3>Johto</h3>
+                </button></li>
+                <li className="nav-item"><button  >
+                  <h3>Hoenn</h3>
+                </button></li>
+                <li className="nav-item"><button className=" text-start " >
+                  <h3>Sinnoh</h3>
+                </button></li>
+                <li className="nav-item"><button  >
+                  <h3>Unova</h3>
+                </button></li>
+                <li className="nav-item"><button  >
+                  <h3>Kalos</h3>
+                </button></li>
+            </div> 
+            <div className="w-50 d-flex "> 
+                <button className="bg-white border-black circulo " onClick={toggleFormulario}><img src="src/assets/img/search.svg" alt="" /></button>
+                {mostrarFormulario && (
+                <form className="w-100 medio ">
+                <input type="text" id={handleSearch} name="campoTexto" className="inform ms-3 "/>
+                </form>
+            )}
+            </div>
+       </header>
+    </div>
+</div> 
--- pokedexDavidAEA2/src/components/navegador.jsx
+++ pokedexDavidAEA2/src/components/navegador.jsx
@@ -10,63 +10,68 @@ export default function Navegador(){
     const toggleFormulario = () => {
         setMostrarFormulario(!mostrarFormulario);
       };
-    const [buscar,setBuscar]=useState('');
-    const URL="https://pokeapi.co/api/v2/pokemon/"+buscar;
-    const getData = async () => {
-        await fetch(URL)
-          .then(response => {return response.json()})
-          .then( data => {
-            if(data.cod >= 400) {
-              setValues(false)
-            }else{         
-              setImagen(data.weather[0].main)
-              setValues(data)
-            }        
-          })
-          .catch(error => {
-            console.log(error)
-          })
-      }
+    
       const handleSearch = (e) => {
         if(e.key === 'Enter'){      
           setBuscar(e.target.value)
         }
       }
-      useEffect(()=>{
-        getData()
-      },[buscar]) 
-    return( 
-    <div className="fixed-top bg-black">
-        <header className="d-flex">
-            <div className="w-75">  
-                <button className=" text-start " >
-                  <h3>Kanto</h3>
-                </button>
-                <button className="  text-start " >
-                  <h3>Johto</h3>
-                </button>
-                <button className="  text-start " >
-                  <h3>Hoenn</h3>
-                </button>
-                <button className=" text-start " >
-                  <h3>Sinnoh</h3>
-                </button>
-                <button className="  text-start " >
-                  <h3>Unova</h3>
-                </button>
-                <button className="  text-start " >
-                  <h3>Kalos</h3>
-                </button>
-            </div> 
-            <div className="w-50 d-flex "> 
+      
+    return(
+      <div className="fixed-top "> 
+    <header>
+    <nav className="navbar navbar-expand-lg bg-black">
+        <div className="container-fluid">
+            <img src="scr/assets/img/pokeball.png" alt="" />
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <button  className="bg-black">
+                            <h3>Kanto</h3>
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button   className="bg-black">
+                            <h3>Johto</h3>
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button   className="bg-black">
+                            <h3>Hoenn</h3>
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button className="bg-black">
+                            <h3>Sinnoh</h3>
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button   className="bg-black">
+                            <h3>Unova</h3>
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button   className="bg-black">
+                            <h3>Kalos</h3>
+                        </button>
+                    </li>
+                </ul>
+                <div className="w-50 d-flex "> 
                 <button className="bg-white border-black circulo " onClick={toggleFormulario}><img src="src/assets/img/search.svg" alt="" /></button>
                 {mostrarFormulario && (
-                <form className="w-100 medio ">
+                <form className="w-100 medio " >
                 <input type="text" id={handleSearch} name="campoTexto" className="inform ms-3 "/>
                 </form>
             )}
             </div>
-       </header>
+            </div>
+        </div>
+    </nav>
+    </header>
     </div>
+
     )
 }
