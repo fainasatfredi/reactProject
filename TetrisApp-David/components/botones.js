2cafffc5403fda48b3e1dd43e2897326c14c2a9a
David Rojas Rodríguez <davidrr.dev@gmail.com>
2024-03-05 21:01:31 +0100

    creado componente botones
    este componente es para los botones que se necesita para jugar al juego del tetris

--- TetrisApp-David/components/botones.js
+++ TetrisApp-David/components/botones.js
@@ -2,8 +2,9 @@ import React from "react";
 import { TouchableOpacity } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 export default function Botones({ name, onPress }){
-        <TouchableOpacity onPress={onPress} style={{ marginHorizontal: 10 }}>
-          <Icon name={name} size={30} color="#000" />
+    return(
+        <TouchableOpacity onPress={onPress} style={{ marginHorizontal: 20 }}>
+          <Icon name={name} size={50} color="#000" />
         </TouchableOpacity>
-
+)
 }
